e7dd61af338c3f70da29f55bbb0fa9305169195c
Martin Desrumaux <martin.desrumaux@epitech.eu>
2018-04-11 16:50:39 +0200

    Adding eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended"
+  ],
+  "rules": {
+     "strict": 0,
+     "indent": [
+         "error",
+         4
+     ],
+     "linebreak-style": [
+         "error",
+         "unix"
+     ],
+     "quotes": [
+         "error",
+         "single"
+     ],
+     "semi": [
+         "error",
+         "never"
+     ]
+  }
+}
