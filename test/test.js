8c75a3a3b37100010d19bd9541e100388c38305d
Daniel Pan <freeplant@gmail.com>
2018-08-02 17:14:00 +0800

    Add jest

--- test/test.js
+++ test/test.js
@@ -2,18 +2,28 @@ const { SeafileAPI } = require('../src/seafile-api')
 
 // load config from config.json
 var fs = require("fs");
-var contents = fs.readFileSync("config.json");
+var contents = fs.readFileSync("test/config.json");
 var config = JSON.parse(contents);
 const repoID = "8342175d-cb2c-457c-adde-66c632c6adc8";
 const filePath = "/test.md"
 const dirPath = "/"
 
-const seafileAPI = new SeafileAPI(config.server, config.username, config.password);
-seafileAPI.login().then((response) => {
+const seafileAPI = new SeafileAPI();
+seafileAPI.init({ server: config.server, username: config.username, password: config.password });
 
-  seafileAPI.authPing().then((response) => {
-    //console.log(response.data);
+beforeAll(() => {
+  return seafileAPI.login();
+});
+
+test("authPing", () => {
+  return seafileAPI.authPing().then((response) => {
+    console.log(response.data);
+    expect(response.data).toBe('pong');
   });
+});
+
+/*
+seafileAPI.login().then((response) => {
 
   seafileAPI.listRepos().then((response) => {
     //console.log(response.data);
@@ -44,5 +54,5 @@ seafileAPI.login().then((response) => {
     //console.log(response.data);
   });
 
-
 })
+*/
