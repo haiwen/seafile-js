1466f95786b977a8dc7c083ca7f134dbb69d1157
Michael An <1822852997@qq.com>
2018-08-07 18:45:50 +0800

    add-some-APIs

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -15,7 +15,7 @@ class SeafileAPI {
         headers: { 'Authorization': 'Token ' + this.token }
       });
     }
-    return this
+    return this;
   }
 
   initForSeahubUsage ({ siteRoot, xcsrfHeaders }) {
@@ -110,7 +110,6 @@ class SeafileAPI {
     return this.req.get(url);
   }
 
-
   starFile(repoID, filePath) {
     const url = this.server + '/api2/starredfiles/';
     let form = new FormData();
@@ -167,9 +166,7 @@ class SeafileAPI {
   }
 
   getFileHistory(repoID, folderPath) {
-
     const url = this.server + "/api2/repos/" + repoID + "/file/history/?p=" + folderPath;
-
     return this.req.get(url);
   }
 
@@ -197,6 +194,83 @@ class SeafileAPI {
     });
   }
 
-}
+  //创建、重命名、删除、移动 文件或者目录(单个文件目录)
 
+  // 测试通过
+  createFile(repoID,filePath){
+    const url = this.server + '/api/v2.1/repos/' + repoID + '/file/?p=' + filePath;
+    let form = new FormData();
+    form.append('operation','create');
+    return this.req.post(url,form,{
+      headers:form.getHeaders()
+    });
+  }
+
+  // 新加入参数：newName
+  renameFile(repoID,filePath,newfileName){
+    const url = this.server + '/api2/repos/' + repoID + '/file/?p=' + filePath;
+    console.log(url);
+    let form = new FormData();
+    form.append('operation','rename');
+    form.append('newname',newfileName);
+    console.log(form);
+    return this.req.post(url,form,{
+      header:form.getHeaders()
+    });
+  }
+  
+  // 测试通过
+  deleteFile(repoID,filePath){
+    const url = this.server + '/api2/repos/' + repoID + '/file/?p=' + filePath;
+    return this.req.delete(url);
+  }
+
+  // 参数: repoID filePath des-repo des-dir(后两个参数需要在测试给出来)
+  copyFile(repoID,filePath,desrepoID,desfilePath){
+    const url = this.server + '/api2/repos/' + repoID + '/file/?p=' + filePath;
+    let form = new FormData();
+    form.append('operation','copy');
+    form.append('des_repo',desrepoID);
+    form.append('des_dir',desfilePath);
+    return this.req.post(url,form,{
+      header:form.getHeaders()
+    });
+  }
+
+  // 参数：新文件目录名-未测试
+  renameDirectory(repoID,folderPath,newdirName){
+    const url = this.server + 'api2/repos/' + repoID + '/dir/?p=' + folderPath;
+    let form = new FormData();
+    form.append("operation",'rename');
+    form.append("newname",newdirName);
+    return this.req.post(url,form,{
+      header:form.getHeaders()
+    });
+    // 函数没有返回值
+  }
+
+  deleteDirectory(repoID,folderPath){
+    const url = this.server + '/api2/repos/' + '/dir/?p=' + folderPath;
+    console.log(url);
+    return this.req.delete(url);
+  }
+
+  // 异步复制/移动文件/目录-未测试
+  // 参数：原始数据库ID，原始父目录，原始目录名称，目标数据库ID，目标父目录
+  moveDirectory(srcrepoID,srcparentDir,srcdirentName,dstrepoID,dstparentDir){
+    const url = this.server + '/api/v2.1/copy-move-task/';
+    let form = new FormData();
+    form.append('src_repo_id', srcrepoID);
+    form.append('src_parent_dir', srcparentDir);
+    form.append('src_dirent_name', srcdirentName);
+    form.append('des_repo_id', dstrepoID);
+    form.append('des_parent_dir', dstparentDir);
+    form.append('operation','move');
+    form.append('dirent_type','dir');
+    return this.req.post(url,form,{
+      header:form.getHeaders()
+    });
+  }
+
+}
 export { SeafileAPI };
--- test/test.js
+++ test/test.js
@@ -4,9 +4,28 @@ const { SeafileAPI } = require('../src/seafile-api')
 var fs = require("fs");
 var contents = fs.readFileSync("test/config.json");
 var config = JSON.parse(contents);
-const repoID = "8342175d-cb2c-457c-adde-66c632c6adc8";
-const filePath = "/test.md"
-const dirPath = "/"
+
+// (原始文件参数)
+// const repoID = "8342175d-cb2c-457c-adde-66c632c6adc8";
+// const filePath = "/test.md";
+// const dirPath = "/";
+
+// 远程文档网站测试
+const repoID = "a46b8d47-8cf9-4c53-8c93-c37e5cecf946";
+const filePath = "/sum.js";
+const dirPath = "/";
+
+const newfileName = "add.js";
+const desrepoID = 'fd12e063-ac3c-45c3-8f3c-81def61da6a7';
+const desfilePath = '/';
+const newdirName = "hello-test";
+
+// const srcrepoID,srcparentDir,srcdirentName,dstrepoID,dstparentDir;
+
+// 本地服务器测试
+// const repoID = "a46b8d47-8cf9-4c53-8c93-c37e5cecf946";
+// const filePath = "/sum.js";
+// const dirPath = "/";
 
 const seafileAPI = new SeafileAPI();
 seafileAPI.init({ server: config.server, username: config.username, password: config.password });
@@ -15,110 +34,171 @@ beforeAll(() => {
   return seafileAPI.login();
 });
 
+// 测试通过
 test("authPing", () => {
   return seafileAPI.authPing().then((response) => {
-    console.log(response.data);
+    // console.log(response.data);
     expect(response.data).toBe('pong');
   });
 });
 
-// test methods in seafile-api.js
-test("login",() => {
-  return seafileAPI.login().then((response) => {
-    console.log(response.data);
-    expect(response.data).not.toBe(null);
-  });
-});
+// 已经在beforeall执行login，不需要测试
+// test("login test",() => {
+//   return seafileAPI.login().then((response) => {
+//     // console.log(response.token);
+//     expect(response).toBe(undefined);
+//   });
+// })
 
 test("listRepos",() => {
   return seafileAPI.listRepos().then((response) => {
-    console.log(response.data);
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("getFileInfo",(repoID,filePath) => {
-  return seafileAPI.getFileInfo().then((response) => {
-    console.log(response.data);
+test("getFileInfo",() => {
+  return seafileAPI.getFileInfo(repoID,filePath).then((response) => {
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
-});
+})
 
-test("getFileDownloadLink",(repoID,filePaht) => {
-  return seafileAPI.getFileDownloadLink().then((response) => {
+// test success
+test("getFileDownloadLink",() => {
+  return seafileAPI.getFileDownloadLink(repoID,filePath).then((response) => {
     var downloadLink = response.data;
-    console.log(response.data);
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("listDir",(repoID,dirPath) => {
-  return seafileAPI.listDir().then((response) => {
-    console.log(response.data);
+// test success
+test("listDir",() => {
+  return seafileAPI.listDir(repoID,dirPath).then((response) => {
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("listDir",(repoID, dirPath, opts = { recursive : true } ) =>{ 
-  return seafileAPI.listDir().then((response) => {
-      console.log(response.data);
+// test success
+test("listDir",() =>{ 
+  return seafileAPI.listDir(repoID, dirPath, true ).then((response) => {
+      // console.log(response.data);
       expect(response.data).not.toBe(null);
   });
 });
 
-test("getUpdateLink",(repoID,folderPath) => {
-  return seafileAPI.getUpdateLink().then((response) => {
-    console.log(response.data);
+// test success
+test("getUpdateLink",() => {
+  return seafileAPI.getUpdateLink(repoID,dirPath).then((response) => {
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("getUploadLink",(repoID,dirPath) => {
-  return seafileAPI.getUploadLink().then((response) => {
-    console.log(response.data);
+// test success
+test("getUploadLink",() => {
+  return seafileAPI.getUploadLink(repoID,dirPath).then((response) => {
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
+// 测试通过
 test("getAccountInfo",() =>{
   return seafileAPI.getAccountInfo().then((response) =>{
-    console.log(response.data);
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("listWikiDir",(slug) =>{
-  return seafileAPI.listWikiDir().then((response) =>{
-    console.log(response.data);
+// 测试通过
+test("getFileHistory test",() => {
+  return seafileAPI.getFileHistory(repoID,filePath).then((response) => {
+    // console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("getWikiFileContent",(slug,filePath) =>{
-  return seafileAPI.getWikiFileContent().then((response) =>{
-    console.log(response.data);
-    expect(response.data).not.toBe(null);
-  });
-});
-
-test("getFileHistory",() => {
-  return seafileAPI.getFileHistory().then((response) => {
-    console.log(response.data);
-    expect(response.data).not.toBe(null);
-  });
-});
-
+// 测试通过
 test("getSharedRepos",() => {
   return seafileAPI.getSharedRepos().then((response) => {
+    // console.log(response.data);
+    expect(response.data).not.toBe(null);
+  });
+});
+
+// 测试通过
+test("getBeSharedRepos",() => {
+  return seafileAPI.getBeSharedRepos().then((response) => {
+    // console.log(response.data);
+    expect(response.data).not.toBe(null);
+  });
+});
+
+// 0807
+
+//test success
+test('createFile test',() => {
+  return seafileAPI.createFile(repoID,filePath).then((response) => {
+    // console.log(response.data.type);
+    expect(response.data.type).toBe('file');
+  });
+});
+
+// error
+test('renameFile test',() => {
+  return seafileAPI.renameFile(repoID,filePath,newfileName).then((response) => {
+    console.log(response.data);
+    expext(response.data).not.toBe(null);
+  });
+});
+
+//test success
+test("deleteFile test",() => {
+  return seafileAPI.deleteFile(repoID,filePath).then((response) => {
+    // console.log(response.data);
+    expect(response.data).not.toBe(null);
+  });
+});
+
+//error
+test('copyFile test',() => {
+  return seafileAPI.copyFile(repoID,filePath,desrepoID,desfilePath).then((response) => {
+    console.log(response.data);
+    expect(response.data).not.tobe(null);
+  });
+}); 
+
+test('createDirecotry test',() => {
+  return seafileAPI.createDirectory(repoID,dirPath).then((response) => {
+    console.log(response.data);
+    expect(response.data).not.toBe(null);
+  });
+});
+    
+test('renameDirectory test',() => {
+  return seafileAPI.renameDirectory(repoID,filePath,newdirName).then((response) => {
+    console.log(response.data);
+    expect(response.data).toBe(null);
+    // 函数没有返回值
+  });
+});
+
+test("deleteDirectory test",() => {
+  return seafileAPI.deleteDirectory(repoID,filePath).then((response) => {
     console.log(response.data);
     expect(response.data).not.toBe(null);
   });
 });
 
-test("getBeSharedRepos",() => {
-  return seafileAPI.getBeSharedRepos().then((response) => {
-    console.log(response.data);
-    expect(response.data).not.toBe(null);
-  });
-});
+// 未给定参数，暂时不测
+// test("moveDirectory,test",() => {
+//   return seafileAPI.moveDirectory(srcrepoID,srcparentDir,srcdirentName,dstrepoID,dstparentDir).then((response) => {
+//     console.log(response.data);
+//     expect(response.data).not.toBe(null);
+//   });
+// });
+
 // test end
