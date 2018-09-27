8d633e24425cd34e899a2189c099d41cb732b555
shanshuirenjia <978987373@qq.com>
2018-09-27 11:08:00 +0800

    add api

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -385,5 +385,20 @@ class SeafileAPI {
     return this.req.get(url);
   }
 
+  zipDownload(repoID, parent_dir, dirents) {
+    const url = this.server + '/api/v2.1/repos/' + repoID + '/zip-task/?parent_dir=' + parent_dir + '&dirents=' + dirents;
+    return this.req.get(url);
+  }
+
+  queryZipProgress(zip_token) {
+    const url = this.server  + '/api/v2.1/query-zip-progress/?token=' + zip_token;
+    return this.req.get(url);
+  }
+
+  cancelZipTask(zip_token) {
+    const url = this.server + '/api/v2.1/cancel-zip-task/?token=' + zip_token;
+    return this.req.get(url);
+  }
+  
 }
 export { SeafileAPI };
