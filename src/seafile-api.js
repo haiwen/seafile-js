8df117b391293895666fac6d0cfb712dc3265f22
wangjianhui <wang@163.com>
2019-01-10 14:33:49 +0800

    add editWikiPermission API

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -1199,6 +1199,14 @@ class SeafileAPI {
     return this._sendPostRequest(url, form);
   }
 
+  editWikiPermission(slug, permission) {
+    const url = this.server + '/api/v2.1/wikis/' + slug + '/';
+    let params = {
+      permission: permission
+    };
+    return this.req.put(url, params);
+  }
+
   deleteWiki(slug) {
     const url = this.server + '/api/v2.1/wikis/' + slug + '/';
     return this.req.delete(url);
