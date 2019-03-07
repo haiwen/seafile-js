75a61d36b98da2f0b7ffa5776c79e0cc3ba4f956
Michael An <2331806369@qq.com>
2019-03-07 17:56:25 +0800

    update edit comment API

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -894,17 +894,10 @@ class SeafileAPI {
 
   updateComment(repoID, commentID, resolved, detail, comment) {
     const url = this.server + '/api2/repos/' + repoID + '/file/comments/' + commentID + '/';
-    let params = {
-      resolved: resolved
-    }
-    if (detail) {
-      params.detail = detail;
-    }
-
-    if (comment) {
-      params.comment = comment;
-    }
-
+    let params = {};
+    if (resolved) params.resolved = resolved;
+    if (detail) params.detail = detail;
+    if (comment) params.comment = comment;
     return this.req.put(url, params);
   }
 
