943d9f0a6108c308b2564921178884fe00fb2c2a
LeoSirius <is.li.xiaoyu@qq.com>
2019-10-31 11:42:58 +0800

    sysadmin institutions api

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -2707,6 +2707,69 @@ class SeafileAPI {
     return this.req.get(url, {params: params});
   }
 
+  sysAdminListInstitutions(page, perPage) {
+    const url = this.server + '/api/v2.1/admin/institutions/';
+    let params = {
+      page: page,
+      per_page: perPage
+    };
+    return this.req.get(url, {params: params});
+  }
+
+  sysAdminAddInstitution(name) {
+    const url = this.server + '/api/v2.1/admin/institutions/';
+    let formData = new FormData();
+    formData.append('name', name);
+    return this._sendPostRequest(url, formData);
+  }
+
+  sysAdminDeleteInstitution(institutionID) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/';
+    return this.req.delete(url);
+  }
+
+  sysAdminGetInstitution(institutionID) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/';
+    return this.req.get(url);
+  }
+
+  sysAdminUpdateInstitution(institutionID, quota) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/';
+    let formData = new FormData();
+    formData.append('quota', quota);
+    return this.req.put(url, formData);
+  }
+
+  sysAdminListInstitutionUsers(institutionID, page, perPage) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/users/';
+    let params = {
+      page: page,
+      per_page: perPage
+    };
+    return this.req.get(url, {params: params});
+  }
+
+  sysAdminListInstitutionAdmins(institutionID) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/admins/';
+    return this.req.get(url);
+  }
+
+  sysAdminAddInstitutionUser(institutionID, emailArray) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/users/';
+    let formData = new FormData();
+    emailArray.map(email => {
+      formData.append('email', email);
+    });
+    return this.req.post(url, formData);
+  }
+
+  sysAdminUpdateInstitutionUser(institutionID, email, isInstitutionAdmin) {
+    const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/users/' + encodeURIComponent(email) + '/';
+    let formData = new FormData();
+    formData.append('is_inst_admin', isInstitutionAdmin);
+    return this.req.put(url, formData);
+  }
+
 }
 
 export { SeafileAPI };
