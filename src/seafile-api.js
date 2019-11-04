fde347db2ecf8965170feeba73e067f700f08b74
Leo <is.li.xiaoyu@qq.com>
2019-11-04 09:52:11 +0800

    optimize seafile js api name sysAdminAddInstitutionUserBatch (#245)

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -2815,7 +2815,7 @@ class SeafileAPI {
     return this.req.get(url, {params: params});
   }
 
-  sysAdminAddInstitutionUser(institutionID, emailArray) {
+  sysAdminAddInstitutionUserBatch(institutionID, emailArray) {
     const url = this.server + '/api/v2.1/admin/institutions/' + institutionID + '/users/';
     let formData = new FormData();
     emailArray.map(email => {
