a3823156d5b4397c64cdf5ff101b9dec9588809f
杨顺强 <ysq9207@126.com>
2020-08-07 10:42:37 +0800

    add list virus scan records (#282)
    * add list virus scan records
    
    * update version
    
    * add delete virus api for 7.1
    
    * update version

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -3098,6 +3098,18 @@ class SeafileAPI {
     return this.req.delete(url);
   }
 
+  // patch 7.1
+  listVirusScanRecords(pageNum) {
+    const url = this.server + '/api/v2.1/admin/virus-scan-records/?page=' + pageNum;
+    return this.req.get(url);
+  }
+  
+  // patch 7.1
+  deleteVirusScanRecord(virusID) {
+    const url = this.server + '/api/v2.1/admin/virus-scan-records/' + virusID + '/';
+    return this.req.delete(url);
+  }
+
   listVirusFiles(page, perPage, hasHandled) {
     const url = this.server + '/api/v2.1/admin/virus-files/';
     let params = {
