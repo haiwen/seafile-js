9d7acd5f2d74eaddf14762e580e592f73ff58d91
lian <imwhatiam123@gmail.com>
2020-11-16 10:44:40 +0800

    search file by name (#290)
    * search file by name
    
    * [search files in repo] fixup and improvement
    
    Co-authored-by: lian <lian@seafile.com>
    Co-authored-by: llj <lingjun.li1@gmail.com>

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -670,8 +670,16 @@ class SeafileAPI {
     return this.req.get(url, {cancelToken : cancelToken});
   }
 
+  searchFileInRepo(repoID, q) {
+    const url = this.server + '/api/v2.1/search-file/';
+    const params = {
+      repo_id: repoID,
+      q: q
+    };
+    return this.req.get(url, {params: params});
+  }
+
   listRepoAPITokens(repo_id) {
-    console.log('server in function is: ', this.server);
     var url = this.server + '/api/v2.1/repos/' + repo_id + '/repo-api-tokens/';
     return this.req.get(url);
   }
