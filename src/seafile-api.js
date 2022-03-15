0f3c6cc5448bce6389639b238d75df54c0e627cf
lian <imwhatiam123@gmail.com>
2022-03-15 11:02:35 +0800

    show tag info on folder share link page (#326)
    * show tag info on folder share link page
    
    * updated the URL for 'getShareLinkTaggedFiles'
    
    Co-authored-by: lian <lian@seafile.com>
    Co-authored-by: llj <lingjun.li1@gmail.com>

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -384,6 +384,16 @@ class SeafileAPI {
     return this.req.get(url);
   }
 
+  getShareLinkRepoTags(token) {
+    var url = this.server + '/api/v2.1/share-links/' + token + '/repo-tags/';
+    return this.req.get(url);
+  }
+
+  getShareLinkTaggedFiles(token, tag_id) {
+    var url = this.server + '/api/v2.1/share-links/' + token + '/tagged-files/' + tag_id + '/';
+    return this.req.get(url);
+  }
+
   // upload-link
   listUserUploadLinks() {
     const url = this.server + '/api/v2.1/upload-links/';
