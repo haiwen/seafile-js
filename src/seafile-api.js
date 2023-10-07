20ca900c5a2ac39ae19afa5cd5f3514c11a25cca
Alex Happy <1223408988@qq.com>
2023-10-07 14:52:01 +0800

    Sync apply folder ex props (#371)
    * remove useless api
    
    * sync apply folder ex-props

--- src/seafile-api.js
+++ src/seafile-api.js
@@ -1250,17 +1250,11 @@ class SeafileAPI {
     return this.req.delete(url);
   }
 
-  setFolderItemsExtendedProperties(repoID, path) {
-    const url = this.server + '/api/v2.1/repos/' + repoID + '/folder-items-extended-properties/';
+  applyFolderExtendedProperties(repoID, path) {
+    const url = this.server + '/api/v2.1/repos/' + repoID + '/apply-folder-extended-properties/';
     var form = new FormData();
     form.append('path', path);
     return this.req.post(url, form);
-  }
-
-  queryFolderItemsExtendedPropertiesStatus(repoID, path) {
-    const url = this.server + '/api/v2.1/repos/' + repoID + '/folder-items-extended-properties/status-query/';
-    var params = { path };
-    return this.req.get(url, { params });
   }
 
   // file commit api
